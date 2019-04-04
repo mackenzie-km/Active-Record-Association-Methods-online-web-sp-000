eb2bd0d5d3b28c14fc36699b9c9716eeab97dbaf
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-04 00:04:36 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,7 +3,7 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    Genre.find_by :name = self.songs.first
+    Genre.find_by name: self.songs.first
   end
 
   def song_count
