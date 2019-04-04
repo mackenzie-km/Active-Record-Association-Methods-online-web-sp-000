a5d6866e77ac9d5b08878eceecc26f97592156d5
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-04 00:48:17 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,8 @@ class Song < ActiveRecord::Base
     self.genre.name
   end
 
-  def drake_made_this
+  def self.drake_made_this
+    binding.pry
     drake = Artist.find_or_create_by(name: "Drake")
       Song.all.collect do |x|
           if x.artist == drake then x end
