ef75644b05822d6729238fc7e2f1d01d315a55f1
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-04 00:26:25 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,6 +7,7 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
+    binding.pry
     drake = Artist.find_by name: 'Drake'
     if drake != nil
       Song.all.map {|x| x.artist_id == drake.id}
