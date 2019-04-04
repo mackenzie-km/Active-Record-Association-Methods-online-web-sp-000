8b84fa9d5624342a64c9745a00beddbfebf40e4f
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-04 00:11:36 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,9 +3,10 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
-    # when this method is called it should assign the song's artist to Drake
+    Song.find_by artist_id: Artist.find_by name: "Drake"
   end
 end
